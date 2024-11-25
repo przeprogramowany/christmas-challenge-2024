2ecf97522fac4adaf8eb65383805d2ab444eae38
przeprogramowany <przeprogramowany.dev@gmail.com>
2024-11-25 21:34:34 +0100

    Task - day 1

---
+++ src/day_1/Task.java
@@ -0,0 +1,20 @@
+package day_1;
+
+/**
+ * Wyzwanie rozpoczynamy od rysowania w konsoli. Napisz program, który wygeneruje
+ * choinkę. Jeśli potrafisz, zrób tak, aby użytkownik mógł podać wysokość drzewka :)
+ * Może ona być po prostu trójkątem albo zawierać dodatkowo jakieś ozdoby i pień.
+ * Wszystko zależy od Ciebie. Powodzenia!
+ */
+
+public class Task {
+
+    public static void main(String[] args) {
+        final int height = 7;
+        drawChristmasTree(height);
+    }
+
+    private static void drawChristmasTree(int height) {
+        // TODO: przygotuj kod, który narysuje drzewko świąteczne w konsoli
+    }
+}
