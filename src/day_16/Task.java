f2a639e34937d63341d4db21727649e4e644d3af
przeprogramowany <przeprogramowany.dev@gmail.com>
2024-12-15 20:32:19 +0100

    Task - day 16 & Solution - day 15

---
+++ src/day_16/Task.java
@@ -0,0 +1,17 @@
+package day_16;
+
+import java.util.List;
+
+/**
+ * Mikołaj zakodował wiadomość w kodzie ASCII. Zamień liczby na litery
+ * i odczytaj hasło, które jest ukryte pod 'SANTA_MESSAGE'.
+ */
+public class Task {
+
+    private static final List<Integer> SANTA_MESSAGE = List.of(87, 101,
+            115, 111, 108, 121, 99, 104, 32, 83, 119, 105, 97, 116, 33);
+
+    public static void main(String[] args) {
+        // TODO - zamień cyfry na litery i odczytaj ukrytą wiadomość
+    }
+}
